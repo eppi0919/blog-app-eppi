7da77ec66fd031785cd2b70f2a4649dcdc66a0ec
eppi0919 <ray.j1.0919@gmail.com>
2022-09-13 02:24:33 +0900

    articleからデータ取得、表示

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,10 +1,6 @@
 class HomeController < ApplicationController
   def index
-    # renderは表示する
-    # home/indexはhomeのindex.html.erb
-    # render 'home/index'を書かなくても実行可
-    # render 'home/index'
-    @title = 'デイトラ'
+    @article = Article.first
   end
 
   def about
